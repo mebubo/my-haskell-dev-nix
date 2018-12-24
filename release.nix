21177000a34f12383338a8f96d42312cdb144e15
Sergei Dolgov <dolgovs@gmail.com>
2018-12-24 19:02:05 +0100

    Add purescript, spago, aoc2018

--- release.nix
+++ release.nix
@@ -11,19 +11,32 @@ let
           th-desugar = self.th-desugar_1_9;
           RSA = dontCheck super.RSA;
           megaparsec = self.megaparsec_7_0_4;
+          protolude = self.protolude_0_2_3;
+          async-pool = doJailbreak super.async-pool;
+          repline = self.repline_0_2_0_0;
+          dhall = dontCheck self.dhall_1_19_1;
+          dhall-json = self.dhall-json_1_2_5;
 
           funflow = self.callPackage ../funflow/funflow {};
           funflow-examples = self.callPackage ../funflow/funflow-examples {};
           advent-of-code-2018 = dontCheck (self.callPackage ../advent-of-code-2018 {});
           codex = doJailbreak (self.callPackage ../codex {});
+          aoc2018 = self.callPackage ../adventofcode/2018 {};
+          purescript = self.callPackage ../purescript {};
+          spago = self.callPackage ../spago {};
         };
       };
     };
   };
   pkgs = import <nixpkgs> { inherit config; };
-in {
-  funflow = pkgs.hp.funflow;
-  funflow-examples = pkgs.hp.funflow-examples;
-  advent-of-code-2018 = pkgs.hp.advent-of-code-2018;
-  codex = pkgs.hp.codex;
+in with pkgs.hp; {
+  inherit
+  funflow
+  funflow-examples
+  advent-of-code-2018
+  codex
+  aoc2018
+  purescript
+  spago
+  ;
 }
